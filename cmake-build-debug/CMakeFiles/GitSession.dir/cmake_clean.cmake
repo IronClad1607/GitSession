e5eea2f11e0192b50eca73af9c828e36b416979d
Ishaan Jain <31792878+IronClad1607@users.noreply.github.com>
2019-08-05 14:39:28 +0530

    Project Started!

---
+++ cmake-build-debug/CMakeFiles/GitSession.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GitSession.dir/main.cpp.obj"
+  "GitSession.pdb"
+  "GitSession.exe"
+  "GitSession.exe.manifest"
+  "libGitSession.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GitSession.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/GitSession.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GitSession.
+# This may be replaced when dependencies are built.
